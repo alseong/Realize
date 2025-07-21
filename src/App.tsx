1c0b63c9a9ac961b0fe29696e9d12da1721780db
mseong6251 <minjun.seong@circleci.com>
2025-07-20 21:53:54 -0400

    React

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+
+export default App;
